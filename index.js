603680d461f49652f050d197d4efb8a0267d7648
MANOJ_SM <manumedleri@gmail.com>
2024-10-01 21:12:31 +0530

    added array search methodes

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
-const ages = [ 3,42,12,23,14,26];
 
-const checkAges = ages.some( age => age >= 18 );    
-                                                                   
-console.log(checkAges); 
+// find - returns the first ele in the array that satisy the condition 
+
+// const arr = [1,2,3,4];
+
+// const is_even = arr.find(ele => ele % 2 ==0);
+// console.log(is_even );                              // 2
+
+//----------------------------------------------------------------------------
+
+
+// findLast - returns the first ele in the array that satisy the condition 
+
+// const arr = [1,2,3,4];
+
+// const is_even = arr.findLast(ele => ele % 2 ==0);
+// console.log(is_even );                                 // 4
+
+
+//----------------------------------------------------------------------------
+
+// findIndex - returns the index of the  first ele which satisy the condition 
+
+// const arr = [1,2,3,4];
+
+// const findIndexofele = arr.findIndex(ele => ele % 2 ==0);
+// console.log(findIndexofele);                                  // 1
+
+//----------------------------------------------------------------------------
+
+// findLastindex - returns the index of the  first ele which satisy the condition 
+
+// const arr = [1,2,3,4];
+
+// const findLastIndexOfele = arr.findLastIndex(ele => ele % 2 ==0);
+// console.log(findLastIndexOfele);                                         // 3
