6e2102c8be69f66e044a408b24ebfa85e3c63ce8
MANOJ_SM <manumedleri@gmail.com>
2024-10-01 20:52:08 +0530

    added every() and some() array methodes

---
+++ index.js
@@ -0,0 +1,5 @@
+const ages = [ 3,42,12,23,14,26];
+
+const checkAges = ages.some( age => age >= 18 );    
+                                                                   
+console.log(checkAges); 
