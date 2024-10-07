c70621005a0fd463fe298e5e161c12361fd2f0fa
MANOJ_SM <manumedleri@gmail.com>
2024-10-07 23:12:40 +0530

    modified

--- oop/index.js
+++ oop/index.js
@@ -1,14 +1,20 @@
+function Person(name) {
+    this.name = name;
+}
 
-
-const user = {
-   name : "manu",
-   city : "sgn",
-   printDetails : function(){
-       console.log(`i am ${this.name} from ${this.city}`);
-   }
+Person.prototype.greet = function() {
+    console.log("Hello, my name is " + this.name);
 };
 
-user.printDetails();
+var person1 = new Person("Alice");
+var person2 = new Person("Bob");
+
+// What will the following code output when executed?
+person1.greet();  // Output: Hello, my name is Alice
+person2.greet();  // Output: Hello, my name is Bob
 
 
-const arr = [ 1,2,3,4];
+// here person is an object and we are adding a custom function to it , greet() , now the instances created using person object will have the this greet method involved 
+
+// person  = function prototype 
+
