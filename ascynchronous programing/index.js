f523f4bc3ff4d4018c2578691b03dc3eeb6206ca
MANOJ_SM <manumedleri@gmail.com>
2024-10-28 21:12:38 +0530

    modified index.js

--- ascynchronous programing/index.js
+++ ascynchronous programing/index.js
@@ -1,22 +1,34 @@
 
 
-console.log("manu");
+function simplePromiseDemo(success) {
+    // Return a Promise
+    return new Promise((Resolve,reject) => {
 
-setTimeout( () => {    
-    console.log("hello world!");        
-},1000);
+    // Log "Promise is pending..."
+    
+    console.log("Promise is pending...");
 
-console.log("manoj");
-console.log("manoj");
-console.log("manoj");
-console.log("manoj");
-console.log("manoj");
-console.log("manoj");
-console.log("manoj");
-console.log("manoj");
-console.log("manoj");
-console.log("manoj");
-console.log("manoj");
-console.log("manoj");
+    // Simulate a network request with setTimeout
+    setTimeout( () => {
+            if(success){
+                Resolve("Promise fulfilled: Data received!");
+            }
+            else{
+                reject("Promise rejected: Error occurred!");
+            }
+        },2000);
+        
+    });
+
+
+}
+    
+    
+
+// Demonstrate fulfilled state
+simplePromiseDemo(true).then( result => console.log(result));
+
+// Demonstrate rejected state
+simplePromiseDemo(false).catch( err => console.log(err));
 
 
