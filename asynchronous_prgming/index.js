e69ff1cb473ed389f0b1d945f9d690e5a64a0c4a
MANOJ_SM <manumedleri@gmail.com>
2024-11-04 22:34:28 +0530

    modified

--- asynchronous_prgming/index.js
+++
@@ -1,9 +0,0 @@
-// https://dummyjson.com/products/1
-
-function fetchData(){
-    fetch(" https://dummyjson.com/products/1")
-    .then(response => response.json())
-    .then(result => console.log(result));
-}
-
-fetchData();
