18f0cff31606e568c6938825788e4d9c39cf4525
MANOJ_SM <manumedleri@gmail.com>
2024-11-11 23:55:59 +0530

    modified

--- asynchronous_prgming/index.js
+++
@@ -1,8 +0,0 @@
-let promiseObject = fetch("https://dummyjson.com/products/1");
-
-console.log(promiseObject);
-
-promiseObject.then( response => response.json()
-                        .then( result => console.log(result)));
-
-promiseObject.catch( ERRresponse => console.log(ERRresponse));
