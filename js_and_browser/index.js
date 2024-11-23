7d978738de83c39e3e37e3f0132bf903adaa0838
MANOJ_SM <manumedleri@gmail.com>
2024-11-23 23:03:14 +0530

    test passed

--- js_and_browser/index.js
+++ js_and_browser/index.js
@@ -1,27 +1,4 @@
-localStorage.setItem("name","manu");
 
-// lenght of localStorage 
-console.log(localStorage.length);
-
-//getting item in localStorage via index no 
-console.log(localStorage.key(0));
-
-//  another way to deleting item from localStorage
-
-localStorage.hobby = "photography";
-delete localStorage.hobby;
-
-// sessionStorage 
-
-sessionStorage.setItem("session","FSD");
-
-const gettingItem = sessionStorage.getItem("session");
-
-console.log(gettingItem);
-
-sessionStorage.removeItem("session");
-
-sessionStorage.clear();
 
 const field = document.getElementById("random");
 
