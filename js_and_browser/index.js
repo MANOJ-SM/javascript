8ed8539eb540e9594fbe60488f3f3176b150853f
MANOJ_SM <manumedleri@gmail.com>
2024-11-23 23:00:10 +0530

    tested testfield into sessionStorage

--- js_and_browser/index.js
+++ js_and_browser/index.js
@@ -1,1 +1,31 @@
-alert("hello");
+localStorage.setItem("name","manu");
+
+// lenght of localStorage 
+console.log(localStorage.length);
+
+//getting item in localStorage via index no 
+console.log(localStorage.key(0));
+
+//  another way to deleting item from localStorage
+
+localStorage.hobby = "photography";
+delete localStorage.hobby;
+
+// sessionStorage 
+
+sessionStorage.setItem("session","FSD");
+
+const gettingItem = sessionStorage.getItem("session");
+
+console.log(gettingItem);
+
+sessionStorage.removeItem("session");
+
+sessionStorage.clear();
+
+const field = document.getElementById("random");
+
+field.addEventListener("change", function(){
+    sessionStorage.setItem("field",field.value);
+});
+
